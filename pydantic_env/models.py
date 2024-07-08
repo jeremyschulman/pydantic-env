8514961df5231ac1c8fe3ff25d4fbdf39a6994f0
Jeremy Schulman <nwkautomaniac@gmail.com>
2024-07-08 17:00:11 -0400

    Jls dev jul01 (#2)

--- pydantic_env/models.py
+++ pydantic_env/models.py
@@ -1,12 +1,33 @@
+from typing_extensions import Annotated
 import os
 from os.path import expandvars
 import re
 from itertools import chain
-from pkg_resources import EntryPoint, iter_entry_points
-from importlib import import_module
-from first import first
+# from pkg_resources import EntryPoint, iter_entry_points
+# from importlib import import_module
+# from first import first
+# from typing import Any
 
-from pydantic import BaseModel, SecretStr, FilePath, AnyHttpUrl, ValidationError
+# from pydantic.main import Model
+from pydantic.types import SecretType
+
+from pydantic import (
+    ConfigDict,
+    BaseModel,
+    SecretStr,
+    FilePath,
+    AnyHttpUrl,
+    ValidationError,
+    RootModel,
+    model_validator,
+)
+
+# from pydantic_core import ValidationError
+from pydantic.functional_validators import (
+    AfterValidator,
+    GetCoreSchemaHandler,
+    core_schema,
+)
 
 _var_re = re.compile(
     r"\${(?P<bname>[a-z0-9_]+)}" r"|" r"\$(?P<name>[^{][a-z_0-9]+)", flags=re.IGNORECASE
@@ -14,11 +35,27 @@ _var_re = re.compile(
 
 
 class NoExtraBaseModel(BaseModel):
-    class Config:
-        extra = "forbid"
+    model_config = ConfigDict(extra="forbid")
 
 
-class EnvExpand(str):
+def exapnd_envvar(v):
+    if found_vars := list(filter(len, chain.from_iterable(_var_re.findall(v)))):
+        for var in found_vars:
+            if (var_val := os.getenv(var)) is None:
+                raise ValueError(f'Environment variable "{var}" missing.')
+
+            if not len(var_val):
+                raise ValueError(f'Environment variable "{var}" empty.')
+
+        return expandvars(v)
+
+    return v
+
+
+MyEnvVar = Annotated[str, AfterValidator(exapnd_envvar)]
+
+
+class _EnvExpand:
     """
     When a string value contains a reference to an environment variable, use
     this type to expand the contents of the variable using os.path.expandvars.
@@ -32,29 +69,50 @@ class EnvExpand(str):
         foo_password -> "boo!_foo"
     """
 
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
+    @staticmethod
+    def _validate(value: str):
+        if value[0] != "$":
+            raise ValueError(f"Value '{value}' does not start with '$'")
+        return value
 
     @classmethod
-    def validate(cls, v):
-        if found_vars := list(filter(len, chain.from_iterable(_var_re.findall(v)))):
-            for var in found_vars:
-                if (var_val := os.getenv(var)) is None:
-                    raise ValueError(f'Environment variable "{var}" missing.')
+    def __get_pydantic_core_schema__(
+        cls, source, handler: GetCoreSchemaHandler
+    ) -> core_schema.CoreSchema:
+        return core_schema.no_info_after_validator_function(
+            cls._validate,
+            core_schema.str_schema(),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda _v: exapnd_envvar(_v),
+                info_arg=False,
+                return_schema=core_schema.str_schema(),
+            ),
+        )
 
-                if not len(var_val):
-                    raise ValueError(f'Environment variable "{var}" empty.')
 
-            return expandvars(v)
+class EnvExpand(RootModel[_EnvExpand]):
+    def __repr__(self):
+        return self.model_dump()
 
-        return v
+    __str__ = __repr__
 
 
-class EnvSecretStr(EnvExpand, SecretStr):
-    @classmethod
-    def validate(cls, v):
-        return SecretStr.validate(EnvExpand.validate(v))
+class EnvSecretStr(SecretStr):
+    """
+    The EnvSecretStr is used to define configuraiton options that store as
+    Secret and validate as AnyHttpUrl
+    """
+
+    def get_secret_value(self) -> SecretType:
+        return exapnd_envvar(super().get_secret_value())
+
+
+def validate_url(url):
+    try:
+        return AnyHttpUrl(url=str(url))
+    except ValidationError as exc:
+        errmsg = exc.errors()[0]["msg"]
+        raise ValueError(f"{errmsg}: {url}")
 
 
 class EnvUrl(EnvExpand):
@@ -63,87 +121,61 @@ class EnvUrl(EnvExpand):
     Secret and validate as AnyHttpUrl
     """
 
-    @classmethod
-    def __get_validators__(cls):
-        yield EnvExpand.validate
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, url):
-
-        # the only way I've realized to validate an AnyHttpUrl field is to
-        # define a model class and then attempt to instantiate an instance.
-        # TODO: find a better way.
-
-        class __T(BaseModel):
-            url: AnyHttpUrl
-
-        try:
-            __T(url=url)
-        except ValidationError as exc:
-            errmsg = exc.errors()[0]["msg"]
-            raise ValueError(f"{errmsg}: {url}")
-
-        return url
+    model_validator(mode="after")(staticmethod(validate_url))
 
 
-class EnvSecretUrl(EnvUrl, EnvSecretStr):
+class EnvSecretUrl(EnvSecretStr):
     """
     The EnvSecretUrl is used to define configuraiton options that store as
     Secret and validate as AnyHttpUrl
     """
 
-    @classmethod
-    def __get_validators__(cls):
-        yield from EnvUrl.__get_validators__()
-        yield EnvSecretStr.validate
+    model_validator(mode="after")(staticmethod(validate_url))
 
 
-class Credential(NoExtraBaseModel):
+class EnvCredential(NoExtraBaseModel):
     username: EnvExpand
     password: EnvSecretStr
 
 
-class FilePathEnvExpand(FilePath):
-    """ A FilePath field whose value can interpolated from env vars """
+class EnvFilePath(EnvExpand):
+    """A FilePath field whose value can interpolate from env vars"""
 
-    @classmethod
-    def __get_validators__(cls):
-        yield from EnvExpand.__get_validators__()
-        yield from FilePath.__get_validators__()
-
-
-class ImportPath(BaseModel):
-    @classmethod
-    def validate(cls, v):
-        try:
-            return import_module(name=v)
-        except ImportError as exc:
-            raise ValueError(f"Unable to import {v}: {str(exc)}")
+    @model_validator(mode="after")
+    @staticmethod
+    def validate_path(value):
+        return FilePath(str(value))
 
 
-class EntryPointImportPath(BaseModel):
-    @classmethod
-    def validate(cls, v):
-        try:
-            return EntryPoint.parse(f"ep = {v}").resolve()
-        except ImportError:
-            raise ValueError(f"Unable to import {v}")
-
-
-class PackagedEntryPoint(BaseModel):
-    @classmethod
-    def validate(cls, v):
-        try:
-            group, _, name = v.partition(":")
-            ep = first(iter_entry_points(group=group, name=name))
-            return ep.resolve()
-
-        except AttributeError:
-            raise ValueError(f"Unable to find package-import: {v}")
-
-        except ImportError as exc:
-            raise ValueError(f"Unable to import {v}: {str(exc)}")
-
-
-
+# class ImportPath(BaseModel):
+#     @classmethod
+#     def validate(cls, v):
+#         try:
+#             return import_module(name=v)
+#         except ImportError as exc:
+#             raise ValueError(f"Unable to import {v}: {str(exc)}")
+#
+#
+# class EntryPointImportPath(BaseModel):
+#     @classmethod
+#     def validate(cls, v):
+#         try:
+#             return EntryPoint.parse(f"ep = {v}").resolve()
+#         except ImportError:
+#             raise ValueError(f"Unable to import {v}")
+#
+#
+# class PackagedEntryPoint(BaseModel):
+#     @classmethod
+#     def validate(cls, v):
+#         try:
+#             group, _, name = v.partition(":")
+#             ep = first(iter_entry_points(group=group, name=name))
+#             return ep.resolve()
+#
+#         except AttributeError:
+#             raise ValueError(f"Unable to find package-import: {v}")
+#
+#         except ImportError as exc:
+#             raise ValueError(f"Unable to import {v}: {str(exc)}")
+#
